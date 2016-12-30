85880cc93e091163769e949955ee2de5945046d0
lvoudour <lymperis.voudouris@gmail.com>
2016-12-30 17:36:42 +0000

    Explicit baud rate and clk frequency Changed description Copyright line instead of entire license

--- src/hdl/uart_tx.vhd
+++ src/hdl/uart_tx.vhd
@@ -1,10 +1,19 @@
 --------------------------------------------------------------------------------
---
 --  UART Tx module
 --
---  8 bit data, 1 stop bit, no parity. Baud rate = Fclk / G_CLK_DIVISOR.
+--  8 bit data, 1 stop bit, no parity. 
 --
---  Assuming clk is the Arty 100 MHz XTAL, the default baud rate will be 1Mbaud
+--  Signals:
+--    clk          : clock of frequency G_CLOCK_FREQ
+--    rst          : active high synchronous reset
+--    tx_en_in     : Set high for at least 1 clk cycle to initate data transfer
+--    tx_data_in   : 8 bit data to send
+--    tx_ready_out : high - module is ready to transfer new data
+--                   low  - module is currently busy and cannot accept new data
+--  Parameters:
+--    G_BAUD_RATE  : UART baud rate
+--    G_CLOCK_FREQ : clk frequency. Can be fractional
+--    
 --  Not optimal for high clk rates/low baud rates, as the dividing counter can
 --  become unnecessarily large.
 --
@@ -12,40 +21,21 @@
 --  and 11 Mbaud.
 --  http://www.ftdichip.com/Support/Documents/DataSheets/ICs/DS_FT2232H.pdf
 --
---  Author(s): Lymperis Voudouris
 --
 --------------------------------------------------------------------------------
--- MIT License
---
--- Copyright (c) 2016 Lymperis Voudouris
--- 
--- Permission is hereby granted, free of charge, to any person obtaining a copy
--- of this software and associated documentation files (the "Software"), to deal
--- in the Software without restriction, including without limitation the rights
--- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
--- copies of the Software, and to permit persons to whom the Software is
--- furnished to do so, subject to the following conditions:
--- 
--- The above copyright notice and this permission notice shall be included in
--- all copies or substantial portions of the Software.
--- 
--- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
--- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
--- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
--- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
--- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
--- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
--- THE SOFTWARE.
---
+--  This work is licensed under the MIT License (see the LICENSE file for terms)
+--  Copyright 2016 Lymperis Voudouris 
 --------------------------------------------------------------------------------
 
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use ieee.math_real.all;
 
 entity uart_tx is
     generic(
-    G_CLK_DIVISOR : positive := 100
+    G_BAUD_RATE  : positive := 1250000;
+    G_CLOCK_FREQ : real     := 100.0e6
     );
     port(
     clk          : in  std_logic;
@@ -59,13 +49,15 @@ end entity uart_tx;
 
 architecture rtl of uart_tx is
     
+    constant C_CLK_DIVISOR : positive := positive(round(G_CLOCK_FREQ / real(G_BAUD_RATE)));
+    
     type fsm_tx_type is (
     FSM_TX_IDLE,
     FSM_TX_SHIFT_WORD
     );
 
     signal fsm_tx_state : fsm_tx_type := FSM_TX_IDLE;
-    signal cnt_div_r    : integer range 0 to G_CLK_DIVISOR-1 := 0;
+    signal cnt_div_r    : integer range 0 to C_CLK_DIVISOR-1 := 0;
     signal cnt_shift_r  : integer range 0 to 9 := 0;
     signal tx_word_sr   : std_logic_vector(9 downto 0) := (others=>'0');
     signal tx_ready_r   : std_logic := '0';
@@ -73,15 +65,15 @@ architecture rtl of uart_tx is
 
 begin
     
-    PROC_FSM_TX:
+    proc_fsm_tx:
     process(clk)
     begin
         if rising_edge(clk) then
             if (rst = '1') then
                 tx_r         <= '1';
                 tx_ready_r   <= '0';
-                cnt_div_r    <= 0;
-                cnt_shift_r  <= 0;
+                cnt_div_r    <=  0;
+                cnt_shift_r  <=  0;
                 fsm_tx_state <= FSM_TX_IDLE;
             else
                 case fsm_tx_state is
@@ -98,7 +90,7 @@ begin
 
                 when FSM_TX_SHIFT_WORD =>
                     tx_r <= tx_word_sr(0);
-                    if (cnt_div_r = G_CLK_DIVISOR-1) then
+                    if (cnt_div_r = C_CLK_DIVISOR-1) then
                         cnt_div_r  <= 0;
                         tx_word_sr <= '1' & tx_word_sr(9 downto 1);
                         if (cnt_shift_r = 9) then
@@ -116,11 +108,6 @@ begin
             end if;
         end if;
     end process;
-
-
----------------------------------
--- Outputs
----------------------------------
 
     tx_ready_out <= tx_ready_r;
     tx_out       <= tx_r;
