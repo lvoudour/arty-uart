273840e26f4558aa780a64e512dd52c0ab33a650
lvoudour <lymperis.voudouris@gmail.com>
2018-04-21 16:46:00 +0100

    Changed indentation to 2 spaces

--- examples/uart-loopback/uart_loopback.vhd
+++ examples/uart-loopback/uart_loopback.vhd
@@ -29,131 +29,131 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity uart_loopback is
-    port(
+  port(
     clk_100mhz_ipad : in  std_logic;
     ck_rst_n_ipad   : in  std_logic;
     uart_rx_ipad    : in  std_logic;
     uart_tx_opad    : out std_logic
-    );
+  );
 end entity uart_loopback;
 
 architecture rtl of uart_loopback is
     
-    ------------------------------------------
-    -- to_slv
-    ------------------------------------------
-    -- Converts a character to std_logic_vector
-    --    c      : Input character
-    --    width  : width of output std_logic_vector
-    function to_slv(c : character; width : positive) return std_logic_vector is
-    begin
-        return std_logic_vector(to_unsigned(character'pos(c), width));
-    end to_slv;
+  ------------------------------------------
+  -- to_slv
+  ------------------------------------------
+  -- Converts a character to std_logic_vector
+  --    c      : Input character
+  --    width  : width of output std_logic_vector
+  function to_slv(c : character; width : positive) return std_logic_vector is
+  begin
+    return std_logic_vector(to_unsigned(character'pos(c), width));
+  end to_slv;
 
-    type fsm_uart_type is (
-    UART_RST,
-    UART_LOOPBACK
-    );
-    signal fsm_uart : fsm_uart_type := UART_RST;
+  type fsm_uart_type is (
+  UART_RST,
+  UART_LOOPBACK
+  );
+  signal fsm_uart : fsm_uart_type := UART_RST;
 
-    signal ck_rst_n_i      : std_logic := '1';
-    signal rst             : std_logic := '0';
-    signal uart_rx_r0      : std_logic := '1';
-    signal uart_rx_r1      : std_logic := '1';
+  signal ck_rst_n_i      : std_logic := '1';
+  signal rst             : std_logic := '0';
+  signal uart_rx_r0      : std_logic := '1';
+  signal uart_rx_r1      : std_logic := '1';
 
-    signal rx_data_rd_sr   : std_logic_vector(1 downto 0) := (others=>'0');
-    signal tx_data_wr_r    : std_logic := '0';
-    signal tx_fifo_full_i  : std_logic := '0';
-    signal rx_fifo_empty_i : std_logic := '0';
-    signal rx_data_i       : std_logic_vector(7 downto 0) := (others=>'0');
-    signal tx_data_r       : std_logic_vector(7 downto 0) := (others=>'0');
+  signal rx_data_rd_sr   : std_logic_vector(1 downto 0) := (others=>'0');
+  signal tx_data_wr_r    : std_logic := '0';
+  signal tx_fifo_full_i  : std_logic := '0';
+  signal rx_fifo_empty_i : std_logic := '0';
+  signal rx_data_i       : std_logic_vector(7 downto 0) := (others=>'0');
+  signal tx_data_r       : std_logic_vector(7 downto 0) := (others=>'0');
 
-    constant MSG : string(1 to 32) := "Arty UART v1.0" & CR & LF & "--------------" & CR & LF;
-    signal cnt_msg_r : integer range 0 to MSG'length-1 := 0;
+  constant MSG : string(1 to 32) := "Arty UART v1.0" & CR & LF & "--------------" & CR & LF;
+  signal cnt_msg_r : integer range 0 to MSG'length-1 := 0;
 begin
     
-    -- External reset is active low. No sync flip-flop needed
-    -- since the reset duration is ~1ms
-    rst <= not ck_rst_n_ipad;
+  -- External reset is active low. No sync flip-flop needed
+  -- since the reset duration is ~1ms
+  rst <= not ck_rst_n_ipad;
 
-    -- Use a double flip-flop to synchronize the rx input
-    -- to the FPGA clock
-    proc_rx_dff:
-    process(clk_100mhz_ipad)
-    begin
-        if rising_edge(clk_100mhz_ipad) then
-            uart_rx_r0 <= uart_rx_ipad;
-            uart_rx_r1 <= uart_rx_r0;
-        end if;
-    end process;
+  -- Use a double flip-flop to synchronize the rx input
+  -- to the FPGA clock
+  proc_rx_dff:
+  process(clk_100mhz_ipad)
+  begin
+    if rising_edge(clk_100mhz_ipad) then
+      uart_rx_r0 <= uart_rx_ipad;
+      uart_rx_r1 <= uart_rx_r0;
+    end if;
+  end process;
 
 
-    proc_loopback:
-    process(clk_100mhz_ipad)
-    begin        
-        if rising_edge(clk_100mhz_ipad) then
-            
-            -- default assignments
-            rx_data_rd_sr <= rx_data_rd_sr(0) & '0';
-            tx_data_wr_r <= '0';
-        
-            if (rst = '1') then
-                cnt_msg_r <= 0;
-                fsm_uart  <= UART_RST;
+  proc_loopback:
+  process(clk_100mhz_ipad)
+  begin        
+    if rising_edge(clk_100mhz_ipad) then
+      
+      -- default assignments
+      rx_data_rd_sr <= rx_data_rd_sr(0) & '0';
+      tx_data_wr_r <= '0';
+  
+      if (rst = '1') then
+        cnt_msg_r <= 0;
+        fsm_uart  <= UART_RST;
+      else
+
+        case fsm_uart is
+
+        -- On reset transmit a "welcome" message before switching
+        -- to loopback mode.
+        when UART_RST =>
+          if (tx_fifo_full_i='0') and (tx_data_wr_r='0') then
+            tx_data_r    <= to_slv(MSG(cnt_msg_r+1), 8);
+            tx_data_wr_r <= '1';
+            if (cnt_msg_r = MSG'length-1) then
+              cnt_msg_r <= 0;
+              fsm_uart  <= UART_LOOPBACK;
             else
-
-                case fsm_uart is
-
-                -- On reset transmit a "welcome" message before switching
-                -- to loopback mode.
-                when UART_RST =>
-                    if (tx_fifo_full_i='0') and (tx_data_wr_r='0') then
-                        tx_data_r    <= to_slv(MSG(cnt_msg_r+1), 8);
-                        tx_data_wr_r <= '1';
-                        if (cnt_msg_r = MSG'length-1) then
-                            cnt_msg_r <= 0;
-                            fsm_uart  <= UART_LOOPBACK;
-                        else
-                            cnt_msg_r <= cnt_msg_r + 1;
-                        end if;
-                    end if;
-
-                -- Read a word from the Rx FIFO and pass it to the Tx FIFO.
-                -- Transaction takes 2 clk cycles.
-                when UART_LOOPBACK =>
-                    tx_data_wr_r <= rx_data_rd_sr(1);
-                    tx_data_r    <= rx_data_i;
-                    if (rx_fifo_empty_i = '0') and (rx_data_rd_sr(0) = '0') then
-                        rx_data_rd_sr <= rx_data_rd_sr(0) & '1';
-                    end if;
-
-                end case;
-
+              cnt_msg_r <= cnt_msg_r + 1;
             end if;
-        end if;
-    end process;
+          end if;
+
+        -- Read a word from the Rx FIFO and pass it to the Tx FIFO.
+        -- Transaction takes 2 clk cycles.
+        when UART_LOOPBACK =>
+          tx_data_wr_r <= rx_data_rd_sr(1);
+          tx_data_r    <= rx_data_i;
+          if (rx_fifo_empty_i = '0') and (rx_data_rd_sr(0) = '0') then
+            rx_data_rd_sr <= rx_data_rd_sr(0) & '1';
+          end if;
+
+        end case;
+
+      end if;
+    end if;
+  end process;
 
 ---------------------------------------------
 -- UART
 ---------------------------------------------
 
-    uart_inst : entity work.uart(rtl)
-        generic map(
-        G_BAUD_RATE  => 1250000,
-        G_CLOCK_FREQ => 100.0e6
-        )
-        port map(
-        clk               => clk_100mhz_ipad,
-        rst               => rst,
-        tx_data_in        => tx_data_r,
-        tx_data_wr_in     => tx_data_wr_r,
-        tx_fifo_full_out  => tx_fifo_full_i,
-        tx_out            => uart_tx_opad,
-        rx_in             => uart_rx_r1,
-        rx_data_rd_in     => rx_data_rd_sr(0),
-        rx_data_out       => rx_data_i,
-        rx_fifo_empty_out => rx_fifo_empty_i
-        );
+  uart_inst : entity work.uart(rtl)
+    generic map(
+      G_BAUD_RATE  => 1250000,
+      G_CLOCK_FREQ => 100.0e6
+    )
+    port map(
+      clk               => clk_100mhz_ipad,
+      rst               => rst,
+      tx_data_in        => tx_data_r,
+      tx_data_wr_in     => tx_data_wr_r,
+      tx_fifo_full_out  => tx_fifo_full_i,
+      tx_out            => uart_tx_opad,
+      rx_in             => uart_rx_r1,
+      rx_data_rd_in     => rx_data_rd_sr(0),
+      rx_data_out       => rx_data_i,
+      rx_fifo_empty_out => rx_fifo_empty_i
+    );
 
 
 end architecture rtl;
--- sim/tb_uart.vhd
+++ sim/tb_uart.vhd
@@ -23,176 +23,176 @@ end entity tb_uart;
 
 architecture behv of tb_uart is  
     
-    ------------------------------------------
-    -- uart_tx
-    ------------------------------------------
-    -- Emulates an external UART device Tx
-    --    txdata : Data to transmit
-    --    tx     : Tx data line
-    --    T_UART : UART period (bit duration)
-    procedure uart_tx ( 
-        variable txdata : in    std_logic_vector(7 downto 0); 
-        signal   tx     : inout std_logic;
-        constant T_UART : in    time) is 
-    begin 
-        tx <= '0'; -- start bit
-        wait for T_UART;
-        for i in 0 to 7 loop 
-            tx <= txdata(i);
-            wait for T_UART;
-        end loop; 
-        tx <= '1'; -- stop bit
-        wait for T_UART;
-    end uart_tx;
+  ------------------------------------------
+  -- uart_tx
+  ------------------------------------------
+  -- Emulates an external UART device Tx
+  --    txdata : Data to transmit
+  --    tx     : Tx data line
+  --    T_UART : UART period (bit duration)
+  procedure uart_tx ( 
+    variable txdata : in    std_logic_vector(7 downto 0); 
+    signal   tx     : inout std_logic;
+    constant T_UART : in    time) is 
+  begin 
+    tx <= '0'; -- start bit
+    wait for T_UART;
+    for i in 0 to 7 loop 
+      tx <= txdata(i);
+      wait for T_UART;
+    end loop; 
+    tx <= '1'; -- stop bit
+    wait for T_UART;
+  end uart_tx;
 
-    ------------------------------------------
-    -- uart_rx
-    ------------------------------------------
-    -- Emulates an external UART device Rx
-    --    rx     : Rx data line
-    --    rxdata : Data received
-    --    T_UART : UART period (bit duration)
-    procedure uart_rx ( 
-          signal   rx     : in  std_logic;
-          variable rxdata : out std_logic_vector(7 downto 0); 
-          constant T_UART : in  time) is 
-    begin
-        wait until falling_edge(rx);
-        wait for T_UART/2;
-        
-        for n in 0 to 7 loop
-            wait for T_UART;
-            rxdata(n) := rx;
-        end loop;
+  ------------------------------------------
+  -- uart_rx
+  ------------------------------------------
+  -- Emulates an external UART device Rx
+  --    rx     : Rx data line
+  --    rxdata : Data received
+  --    T_UART : UART period (bit duration)
+  procedure uart_rx ( 
+    signal   rx     : in  std_logic;
+    variable rxdata : out std_logic_vector(7 downto 0); 
+    constant T_UART : in  time) is 
+  begin
+    wait until falling_edge(rx);
+    wait for T_UART/2;
+    
+    for n in 0 to 7 loop
+      wait for T_UART;
+      rxdata(n) := rx;
+    end loop;
 
-        wait for T_UART;
-        assert (rx = '1') report "Incorrect UART stop bit" severity error;
-    end uart_rx;
+    wait for T_UART;
+    assert (rx = '1') report "Incorrect UART stop bit" severity error;
+  end uart_rx;
 
 
-    constant C_CLK_PERIOD  : time := 10 ns;  -- 100 MHz
-    constant C_UART_PERIOD : time := 800 ns; -- 1.25 Mbaud
+  constant C_CLK_PERIOD  : time := 10 ns;  -- 100 MHz
+  constant C_UART_PERIOD : time := 800 ns; -- 1.25 Mbaud
 
-    signal clk             : std_logic := '0';
-    signal rst             : std_logic := '0';
-    signal tx_i            : std_logic := '1';
-    signal tx_data_i       : std_logic_vector(7 downto 0) := (others=>'0');
-    signal tx_data_wr_i    : std_logic := '0';
-    signal tx_fifo_full_i  : std_logic := '0';
-    signal rx_i            : std_logic := '1';
-    signal rx_data_i       : std_logic_vector(7 downto 0) := (others=>'0');
-    signal rx_data_rd_i    : std_logic := '0';
-    signal rx_fifo_empty_i : std_logic := '0';
+  signal clk             : std_logic := '0';
+  signal rst             : std_logic := '0';
+  signal tx_i            : std_logic := '1';
+  signal tx_data_i       : std_logic_vector(7 downto 0) := (others=>'0');
+  signal tx_data_wr_i    : std_logic := '0';
+  signal tx_fifo_full_i  : std_logic := '0';
+  signal rx_i            : std_logic := '1';
+  signal rx_data_i       : std_logic_vector(7 downto 0) := (others=>'0');
+  signal rx_data_rd_i    : std_logic := '0';
+  signal rx_fifo_empty_i : std_logic := '0';
 
-    signal transmitted_text : string(1 to 9) := "TEST_1234";
+  signal transmitted_text : string(1 to 9) := "TEST_1234";
 
 begin
 
-    clk <= not clk after C_CLK_PERIOD/2;
-    rst <= '1', '0' after 1000 ns;
+  clk <= not clk after C_CLK_PERIOD/2;
+  rst <= '1', '0' after 1000 ns;
+  
+  -- Loopback. Connect rx fifo output to tx fifo input
+  tx_data_i <= rx_data_i;
+
+  -- External device UART transmitter
+  proc_external_uart_tx:
+  process
+    variable txdata : std_logic_vector(7 downto 0) := (others=>'0');
+  begin
+    wait until falling_edge(rst);
     
-    -- Loopback. Connect rx fifo output to tx fifo input
-    tx_data_i <= rx_data_i;
+    -- transmit each character of the string (least significant char first)
+    for n in transmitted_text'range loop
+      wait for 133 ns; -- wait some arbitrary amount of time
+      txdata := std_logic_vector(to_unsigned(character'pos(transmitted_text(n)), 8));
+      uart_tx(txdata, rx_i, C_UART_PERIOD);
+    end loop;
+    wait;
+  end process;
 
-    -- External device UART transmitter
-    proc_external_uart_tx:
-    process
-        variable txdata : std_logic_vector(7 downto 0) := (others=>'0');
-    begin
-        wait until falling_edge(rst);
-        
-        -- transmit each character of the string (least significant char first)
-        for n in transmitted_text'range loop
-            wait for 133 ns; -- wait some arbitrary amount of time
-            txdata := std_logic_vector(to_unsigned(character'pos(transmitted_text(n)), 8));
-            uart_tx(txdata, rx_i, C_UART_PERIOD);
-        end loop;
-        wait;
-    end process;
+  -- Read/Write UART Rx/Tx FIFOs
+  proc_loopback:
+  process
+  begin
+    wait until falling_edge(rst);
+    
+    -- Repeat for every character
+    for n in transmitted_text'range loop
 
-    -- Read/Write UART Rx/Tx FIFOs
-    proc_loopback:
-    process
-    begin
-        wait until falling_edge(rst);
-        
-        -- Repeat for every character
-        for n in transmitted_text'range loop
+      -- wait until rx fifo has some data
+      if (rx_fifo_empty_i='1') then
+        wait until rx_fifo_empty_i = '0';
+      end if;
+      
+      -- Read pulse
+      wait until rising_edge(clk);        
+      rx_data_rd_i <= '1';
+      wait until rising_edge(clk);
+      rx_data_rd_i <= '0';
+      
+      -- check if tx fifo is full before writing
+      -- any data (not really necessary in loopback
+      -- configuration)
+      if (tx_fifo_full_i = '1') then
+        wait until tx_fifo_full_i = '0';
+      end if;
 
-            -- wait until rx fifo has some data
-            if (rx_fifo_empty_i='1') then
-                wait until rx_fifo_empty_i = '0';
-            end if;
-            
-            -- Read pulse
-            wait until rising_edge(clk);        
-            rx_data_rd_i <= '1';
-            wait until rising_edge(clk);
-            rx_data_rd_i <= '0';
-            
-            -- check if tx fifo is full before writing
-            -- any data (not really necessary in loopback
-            -- configuration)
-            if (tx_fifo_full_i = '1') then
-                wait until tx_fifo_full_i = '0';
-            end if;
+      -- Write pulse
+      wait until rising_edge(clk);
+      tx_data_wr_i <= '1';
+      wait until rising_edge(clk);
+      tx_data_wr_i <= '0';
 
-            -- Write pulse
-            wait until rising_edge(clk);
-            tx_data_wr_i <= '1';
-            wait until rising_edge(clk);
-            tx_data_wr_i <= '0';
+    end loop;
+    
+    wait;
+  end process;
 
-        end loop;
-        
-        wait;
-    end process;
+  -- External device UART receiver
+  proc_external_uart_rx:
+  process
+    variable rxdata : std_logic_vector(7 downto 0) := (others=>'0');
+    variable received_text : string(transmitted_text'range);
+  begin
+    -- Receive characters and store them in a string
+    for n in transmitted_text'range loop
+      uart_rx(tx_i, rxdata, C_UART_PERIOD);
+      received_text(n) := character'val(to_integer(unsigned(rxdata)));
+    end loop;
 
-    -- External device UART receiver
-    proc_external_uart_rx:
-    process
-        variable rxdata : std_logic_vector(7 downto 0) := (others=>'0');
-        variable received_text : string(transmitted_text'range);
-    begin
-        -- Receive characters and store them in a string
-        for n in transmitted_text'range loop
-            uart_rx(tx_i, rxdata, C_UART_PERIOD);
-            received_text(n) := character'val(to_integer(unsigned(rxdata)));
-        end loop;
+    -- Fail simulation if received text is not equal to the transmitted text
+    assert (received_text = transmitted_text) 
+      report "Received text: " & received_text & " is not equal to trasmitted text: " & transmitted_text
+      severity failure;
 
-        -- Fail simulation if received text is not equal to the transmitted text
-        assert (received_text = transmitted_text) 
-            report "Received text: " & received_text & " is not equal to trasmitted text: " & transmitted_text
-            severity failure;
+    -- All is well. Report success
+    assert false
+      report "Successfuly received transmitted text: " & received_text
+      severity note;
 
-        -- All is well. Report success
-        assert false
-            report "Successfuly received transmitted text: " & received_text
-            severity note;
-
-        wait;
-    end process;
+    wait;
+  end process;
 
 
 ------------------------------------------------
 -- UART
 ------------------------------------------------
-    uart_inst : entity work.uart(rtl)
-        generic map(
-        G_BAUD_RATE  => 1250000,
-        G_CLOCK_FREQ => 100.0e6
-        )
-        port map(
-        clk               => clk,
-        rst               => rst,
-        tx_data_in        => tx_data_i,
-        tx_data_wr_in     => tx_data_wr_i,
-        tx_fifo_full_out  => tx_fifo_full_i,
-        tx_out            => tx_i,
-        rx_in             => rx_i,
-        rx_data_rd_in     => rx_data_rd_i,
-        rx_data_out       => rx_data_i,
-        rx_fifo_empty_out => rx_fifo_empty_i
-        );
+  uart_inst : entity work.uart(rtl)
+    generic map(
+      G_BAUD_RATE  => 1250000,
+      G_CLOCK_FREQ => 100.0e6
+    )
+    port map(
+      clk               => clk,
+      rst               => rst,
+      tx_data_in        => tx_data_i,
+      tx_data_wr_in     => tx_data_wr_i,
+      tx_fifo_full_out  => tx_fifo_full_i,
+      tx_out            => tx_i,
+      rx_in             => rx_i,
+      rx_data_rd_in     => rx_data_rd_i,
+      rx_data_out       => rx_data_i,
+      rx_fifo_empty_out => rx_fifo_empty_i
+    );
 
 end architecture behv;
